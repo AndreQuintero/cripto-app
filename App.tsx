99c8144da204b06117bd76c0d6d81536b6b8bd8a
Gabriela Rodrigues <andre.quintero96@gmail.com>
2021-10-05 22:49:56 -0300

    tirando imports nao utilizados

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout, Button } from '@ui-kitten/components';
 import styled from 'styled-components/native'
