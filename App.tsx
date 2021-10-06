9b4d3e0321223fe7edd132223f4df3bb34594421
Gabriela Rodrigues <andre.quintero96@gmail.com>
2021-10-05 22:24:05 -0300

    hello world

--- App.tsx
+++ App.tsx
@@ -1,21 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Button } from '@ui-kitten/components';
+import styled from 'styled-components/native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApplicationProvider {...eva} theme={{ ...eva.dark }}>
+      <LayoutStyled>
+        <Button>HOME</Button>
+        <StatusBar style="auto" />
+      </LayoutStyled>
+    </ApplicationProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const LayoutStyled = styled(Layout)`
+    flex: 1;
+    justifyContent: center; 
+    alignItems: center 
+`
