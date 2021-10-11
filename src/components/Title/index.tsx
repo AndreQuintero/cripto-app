2643e5742cbfda3f63c64444d3ebff0b255cd5e3
Gabriela Rodrigues <andre.quintero96@gmail.com>
2021-10-11 15:25:44 -0300

    componente de titulo

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import * as S from './style'
+
+type TitleProps = {
+    title: string
+    description?: string
+}
+
+const Title = ({title, description = ''}: TitleProps) => (
+    <S.View>
+        <S.Title>{title}</S.Title>
+        <S.Description>{description}</S.Description>
+    </S.View>
+)
+
+export default Title
---
+++ src/components/Title/style.ts
@@ -0,0 +1,17 @@
+import { Layout, Text as KText} from "@ui-kitten/components"
+import styled from "styled-components/native"
+
+export const View = styled(Layout)`
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Title = styled(KText)`
+    font-size: 24px;
+`
+
+export const Description = styled(KText)`
+    font-size: 16px;
+    font-weight: 300; 
+`
