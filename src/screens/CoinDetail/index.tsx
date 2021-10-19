a2dbfbd7231ee141efff6a6f2cb75d8e05bb7855
Gabriela Rodrigues <andre.quintero96@gmail.com>
2021-10-18 23:32:00 -0300

    implementacao inicial da tela de detalhe da moeda

---
+++ src/screens/CoinDetail/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+  RootStackParamList,
+  CoinModuleParamList
+} from 'config/routes-type.config'
+import * as S from './style'
+import { Button } from '@ui-kitten/components'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+
+type Props = NativeStackScreenProps<CoinModuleParamList, 'CoinDetail'>
+
+const CoinDetail = ({ navigation }: Props) => {
+  return (
+    <S.View>
+      <S.Text>Coin Detail</S.Text>
+      <Button onPress={() => navigation.navigate('CriptoCoins')}>
+        Navegar
+      </Button>
+    </S.View>
+  )
+}
+
+export default CoinDetail
