4f9c129d2fc77058574aa05c2433851d64828d77
Gabriela Rodrigues <andre.quintero96@gmail.com>
2021-10-18 23:35:11 -0300

    adicionando estilo no safe area view e importando arquivo de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,22 @@ import React from 'react'
 import * as eva from '@eva-design/eva'
 import { ApplicationProvider } from '@ui-kitten/components'
 import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import CoinScreen from 'screens/Coins'
-import ExchangesScreen from 'screens/Exchanges'
-import { RootStackParamList } from 'config/routes-type.config'
+
 import { registerRootComponent } from 'expo'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import Routes from 'components/Routes'
 
 function App() {
-  const Stack = createNativeStackNavigator<RootStackParamList>()
   return (
     <ApplicationProvider {...eva} theme={{ ...eva.dark }}>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{ animation: 'slide_from_right', headerShown: false }}
-          initialRouteName="Coins"
-        >
-          <Stack.Screen name="Coins">
-            {(props) => <CoinScreen {...props} />}
-          </Stack.Screen>
-          <Stack.Screen name="Exchanges">
-            {(props) => <ExchangesScreen {...props} />}
-          </Stack.Screen>
-        </Stack.Navigator>
-      </NavigationContainer>
-      <StatusBar style="auto" />
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#301934' }}>
+          <NavigationContainer>
+            <Routes />
+          </NavigationContainer>
+          <StatusBar style="light" />
+        </SafeAreaView>
+      </SafeAreaProvider>
     </ApplicationProvider>
   )
 }
