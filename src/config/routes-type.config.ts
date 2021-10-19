0b94fe323955fa6d935d2f969a4f29eb25900ef0
Gabriela Rodrigues <andre.quintero96@gmail.com>
2021-10-18 23:31:00 -0300

    novos tipos para rotas

--- src/config/routes-type.config.ts
+++ src/config/routes-type.config.ts
@@ -2,3 +2,13 @@ export type RootStackParamList = {
   Coins: undefined
   Exchanges: undefined
 }
+
+export type RootDrawerParamList = {
+  Coins: undefined
+  Exchanges: undefined
+}
+
+export type CoinModuleParamList = {
+  CriptoCoins: undefined
+  CoinDetail: undefined
+}
